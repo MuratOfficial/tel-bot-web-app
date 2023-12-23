1190fa07b1c035ac3aff232396902342a475d077
MuratOfficial <toimetm@gmail.com>
2023-12-23 16:41:29 +0600

    added back function

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,13 +3,21 @@ import Button from '../Button/Button';
 import {useTelegram} from '../../hooks/useTelegram';
 import {FaUser} from "react-icons/fa"
 import "./Header.css"
+import { useLocation } from 'react-router';
+import { Link } from 'react-router-dom';
 
 
 export default function Header() {
-    const {onClose, user} = useTelegram();
+    const {onClose,  user} = useTelegram();
+    const location = useLocation().pathname;
+
     return (
     <div className={'header'}>
-        <Button onClick={onClose}>Закрыть</Button>
+      {location === "/" ? (<Button onClick={onClose}>Закрыть</Button>):(
+      <Link to={"/"}><Button> Назад</Button></Link>
+      )}
+        
+        
         <span className={'username'}>
         <FaUser/> {" "} {user?.username} 
         </span>
