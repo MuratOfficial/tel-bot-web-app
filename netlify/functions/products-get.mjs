e989c4d1e3e855d9756fded95788f11c24f84e65
MuratOfficial <toimetm@gmail.com>
2023-12-25 22:31:53 +0600

    added functions

--- netlify/functions/products-get.mjs
+++ netlify/functions/products-get.mjs
@@ -1,25 +1,22 @@
-const axios = require("axios");
+export default async function () {
+  const apiUrl = "https://api.moysklad.ru/api/remap/1.2/entity/product";
+  const username = "admin@dessert1";
+  const password = "7212565689";
 
-export default async (req, res) => {
-  try {
-    const apiUrl = "https://api.moysklad.ru/api/remap/1.2/entity/product";
-    const username = "admin@dessert1";
-    const password = "7212565689";
+  const credentials = Buffer.from(`${username}:${password}`, "utf-8").toString(
+    "base64"
+  );
 
-    const credentials = Buffer.from(
-      `${username}:${password}`,
-      "utf-8"
-    ).toString("base64");
+  const headers = {
+    Authorization: `Basic ${credentials}`,
+    // Remove "Accept-Encoding": "gzip" - let the browser handle compression
+  };
 
-    const headers = {
-      Authorization: `Basic ${credentials}`,
-      // Remove "Accept-Encoding": "gzip" - let the browser handle compression
-    };
+  const response = await fetch(apiUrl, { headers });
+  const data = await response.json();
 
-    const response = await axios.get(apiUrl, { headers });
-    res.json(response.data);
-  } catch (error) {
-    console.error("Error in proxy request:", error.message);
-    res.status(500).send("Internal Server Error");
-  }
-};
+  return new Response(JSON.stringify(data), {
+    headers: { "Content-Type": "application/json" },
+    status: response.status,
+  });
+}
